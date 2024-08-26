e8790b4663833c00c8ab1942c2b758ae47fae8d3
Josiah Parrott <josiahparrott@gmail.com>
2024-08-26 01:27:26 -0400

    Added Spotify OAuth

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
 	"io"
 	"log/slog"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"crypto/rand"
+	"net/url"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,6 +25,100 @@ type TemplateRenderer struct {
 	templates *template.Template
 }
 
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int    `json:"expires_in"`
+}
+
+type Secrets struct {
+	ClientID     string `json:"clientID"`
+	ClientSecret string `json:"clientSecret"`
+}
+
+var (
+	clientID     = ""
+	clientSecret = ""
+	redirectURI  = "http://localhost:3000/callback"
+	baseURI      = "/"
+)
+
+// Controller
+func logIn(c echo.Context) error {
+	readSecrets()
+
+	state := generateRandomString(16)
+	scope := "user-read-private user-read-email"
+
+	params := url.Values{}
+	params.Add("response_type", "code")
+	params.Add("client_id", clientID)
+	params.Add("scope", scope)
+	params.Add("redirect_uri", redirectURI)
+	params.Add("state", state)
+
+	authURL := "https://accounts.spotify.com/authorize?" + params.Encode()
+	return c.Redirect(http.StatusFound, authURL)
+
+}
+
+// Controller
+func callBack(c echo.Context) error {
+
+	params := url.Values{}
+	params.Add("code", c.Request().URL.Query()["code"][0])
+	params.Add("redirect_uri", redirectURI)
+	params.Add("grant_type", "authorization_code")
+
+	tokenURL := "https://accounts.spotify.com/api/token"
+
+	req, _ := http.NewRequest(http.MethodPost, tokenURL, strings.NewReader(params.Encode()))
+
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(clientID+":"+clientSecret)))
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var tokenHandler TokenResponse
+	err = json.NewDecoder(res.Body).Decode(&tokenHandler)
+	if err != nil {
+		return err
+	}
+
+	cookie := new(http.Cookie)
+	cookie.Name = "Playgen-Access-Token"
+	cookie.Value = tokenHandler.AccessToken
+	cookie.Expires = time.Now().Add(time.Hour)
+
+	c.SetCookie(cookie)
+
+	return c.Redirect(http.StatusFound, baseURI)
+}
+
+// Util
+func generateRandomString(n int) string {
+	b := make([]byte, n)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
+}
+
+// Util
+func readSecrets() {
+	file, _ := os.Open("secrets.json")
+
+	var secrets Secrets
+
+	//err :=
+	json.NewDecoder(file).Decode(&secrets)
+
+	clientID = secrets.ClientID
+	clientSecret = secrets.ClientSecret
+}
+
+// Service
 func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	tmpl, err := t.templates.Clone()
 	if err != nil {
@@ -46,10 +149,6 @@ func main() {
 	port := flag.Int("p", 3000, "port")
 	e := echo.New()
 
-	router := http.NewServeMux()
-
-	router.HandleFunc("POST /login", logIn)
-
 	renderer := &TemplateRenderer{
 		templates: template.Must(template.ParseGlob("templates/*.gotmpl")),
 	}
@@ -62,11 +161,8 @@ func main() {
 	e.Static("/", "static")
 
 	e.GET("/", home)
+	e.GET("/login", logIn)
+	e.GET("/callback", callBack)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", *port)))
-
-}
-
-func logIn(w http.ResponseWriter, r *http.Request) {
-
 }
---
+++ static/js/genre-tags.js
@@ -0,0 +1,58 @@
+const input_tag = document.querySelector(".input-tag")
+
+let tags_list = []
+
+input_tag.addEventListener("keyup", (e) => {
+	const val = input_tag.value
+	if (e.key == "Enter") {
+		if (tags_list.length >= 5) return;
+		if (tags_list.some(e => e.text == val)) return;
+		if (val == "") return;
+		
+		//if val is not in list, return
+
+		const tags = val.split(',').map(e => e.trim()).filter(e => e !== "")
+
+		for (let i of tags) {
+			tags_list.push({
+				id: i,
+				text: i
+			})
+		}
+		input_tag.value = ""
+		RenderTags()
+	}
+})
+
+function RenderTags() {
+	const genre_tags = document.querySelector(".genre-tags")
+	let cache = ""
+
+	document.querySelectorAll(".genre-tag").forEach(e => e.remove())
+	cache = ""
+
+	tags_list.forEach(e => {
+		cache = `<div class = "genre-tag">
+				<span>${e.text}</span>
+				<button type="button" data-id="${e.id}"class="btn-rm-tag">
+					<i class="ti ti-x"></i>
+				</button>
+			</div>`;
+		genre_tags.insertAdjacentHTML("afterbegin", cache)
+		HandleRmTags()
+	})
+}
+
+function HandleRmTags() {
+	const btns = document.querySelectorAll('.btn-rm-tag')
+	if (btns.length > 0) {
+		btns.forEach(e => {
+			e.onclick = function () {
+				const data_id = e.getAttribute('data-id')
+				tags_list = tags_list.filter(x => x.id != data_id)
+				RenderTags();
+			}
+		})
+	}
+
+}
